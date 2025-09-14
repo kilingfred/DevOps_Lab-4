207e9bd3f6797a58d29c35c603fdc280e6118b03
kilingfred <mr.perepolkin@gmail.com>
2025-09-14 20:15:08 +0300

    Forth of many

--- node.js
+++ node.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { env } = require("process");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const fileUpload = require("express-fileupload");
-const port = env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const cloudinary = require("cloudinary");
-const HOSTNAME = env.HOSTNAME || "0.0.0.0";
+const HOSTNAME = process.env.HOSTNAME || "0.0.0.0";
 
-require("dotenv").config();
 app.use(fileUpload({ useTempFiles: true }));
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -24,6 +24,6 @@ app.post("/upload", (req, res) => {
     .then((result) => console.log(result));
 });
 app.get("/", (req, res) => res.render("index", { title: "Hello World!" }));
-app.listen(port, HOSTNAME, () =>
+app.listen(PORT, HOSTNAME, () =>
   console.log(`Server running at http://${HOSTNAME}:${PORT}/`)
 );
